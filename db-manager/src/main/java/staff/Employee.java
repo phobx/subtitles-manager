141d811dfbde2f13ea57029a8bad5bfef62fd149
phobx <gulaga.yaroslav@gmail.com>
2015-01-21 15:53:49 +0200

    21.01.2015

---
+++ db-manager/src/main/java/staff/Employee.java
@@ -0,0 +1,17 @@
+package staff;
+
+import java.util.Date;
+
+public class Employee {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int title;
+	private String email;
+	private String skype;
+	private String workingDirectory;
+	private Date birthDay;
+	private boolean isHired;
+
+}
